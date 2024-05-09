752e7a258ceda691cab9e29a66693a91aefb15f1
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-09 13:52:10 +0530

    debugging

--- photo-generator.js
+++ photo-generator.js
@@ -3,14 +3,17 @@
 // this the attempts variable we will use for the number of attempts 
 // x/6. min value can be 1, that is in case if someone does it in their
 // first try etc.
-const attempts = 1;
+var attempts = 1;
 var isEmpty = false;
+
+var row_full = []
 
 // game board that contains all the rows:
 const board = document.querySelector("#wordle-app-game > div.Board-module_boardContainer__TBHNL > div");
 // board.children.length = 6 always. because there are always 6 rows
 for(var i = 0; i<6; i++){
     // now we are in the current row
+    var row_image = ''
     var row = board.children[i];
     for (var j = 0; j<5; j++){
         var tile = row.children[j].firstChild
@@ -25,19 +28,21 @@ for(var i = 0; i<6; i++){
         // Get the background color property from the computed styles
         const backgroundColor = computedStyles.backgroundColor;
         if (backgroundColor == 'rgb(83, 141, 78)'){
-            console.log('🟩')
+            row_image += '🟩';
         } else if (backgroundColor == 'rgb(181, 159, 59)'){
-            console.log('🟨')
+            row_image += '🟨';
         } else {
-            console.log('⬛')
+            row_image += '⬛';
         }
     }
 
+    row_full.push(row_image)
 
     if (isEmpty){
         break
     }
 }
 
+row_full.pop()
+console.log(row_full)
 attempts = i;
-console.log(str(attempts)+'/6')
