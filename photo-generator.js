f7c35320347c580b4261e618ed1b56e727633c17
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-09 13:38:16 +0530

    code

--- photo-generator.js
+++ photo-generator.js
@@ -1,2 +1,43 @@
 // core js logic for the functioning of the extension
 
+// this the attempts variable we will use for the number of attempts 
+// x/6. min value can be 1, that is in case if someone does it in their
+// first try etc.
+const attempts = 1;
+var isEmpty = false;
+
+// game board that contains all the rows:
+const board = document.querySelector("#wordle-app-game > div.Board-module_boardContainer__TBHNL > div");
+// board.children.length = 6 always. because there are always 6 rows
+for(var i = 0; i<6; i++){
+    // now we are in the current row
+    var row = board.children[i];
+    for (var j = 0; j<5; j++){
+        var tile = row.children[j].firstChild
+
+        // check if tile is empty, if it is then stop the code
+        if (tile.dataset.state == 'empty'){
+            isEmpty = true;
+            break
+        }
+
+        const computedStyles = window.getComputedStyle(tile);
+        // Get the background color property from the computed styles
+        const backgroundColor = computedStyles.backgroundColor;
+        if (backgroundColor == 'rgb(83, 141, 78)'){
+            console.log('🟩')
+        } else if (backgroundColor == 'rgb(181, 159, 59)'){
+            console.log('🟨')
+        } else {
+            console.log('⬛')
+        }
+    }
+
+
+    if (isEmpty){
+        break
+    }
+}
+
+attempts = i;
+console.log(str(attempts)+'/6')
