de1589718ee6e065f5ed5c83ecce86e83b0dc445
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-10 10:37:35 +0530

    debugging

--- photo-generator.js
+++ photo-generator.js
@@ -1,60 +1,70 @@
-// core js logic for the functioning of the extension
+function photo_generator(){    
+    
+    // core js logic for the functioning of the extension
 
-// this the attempts variable we will use for the number of attempts 
-// x/6. min value can be 1, that is in case if someone does it in their
-// first try etc.
-var attempts = 1;
-var isEmpty = false;
+    // this the attempts variable we will use for the number of attempts 
+    // x/6. min value can be 1, that is in case if someone does it in their
+    // first try etc.
+    var attempts = 1;
+    var isEmpty = false;
 
-var row_full = []
+    var row_full = []
 
-// game board that contains all the rows:
-const board = document.querySelector("#wordle-app-game > div.Board-module_boardContainer__TBHNL > div");
-// board.children.length = 6 always. because there are always 6 rows
-for(var i = 0; i<6; i++){
-    // now we are in the current row
-    var row_image = ''
-    var row = board.children[i];
-    for (var j = 0; j<5; j++){
-        var tile = row.children[j].firstChild
+    // game board that contains all the rows:
+    const board = document.querySelector("#wordle-app-game > div.Board-module_boardContainer__TBHNL > div");
+    // board.children.length = 6 always. because there are always 6 rows
+    for(var i = 0; i<6; i++){
+        // now we are in the current row
+        var row_image = ''
+        var row = board.children[i];
+        for (var j = 0; j<5; j++){
+            var tile = row.children[j].firstChild
 
-        // check if tile is empty, if it is then stop the code
-        if (tile.dataset.state == 'empty'){
-            isEmpty = true;
+            // check if tile is empty, if it is then stop the code
+            if (tile.dataset.state == 'empty'){
+                isEmpty = true;
+                break
+            }
+
+            const computedStyles = window.getComputedStyle(tile);
+            // Get the background color property from the computed styles
+            const backgroundColor = computedStyles.backgroundColor;
+            if (backgroundColor == 'rgb(83, 141, 78)'){
+                row_image += '🟩';
+            } else if (backgroundColor == 'rgb(181, 159, 59)'){
+                row_image += '🟨';
+            } else {
+                row_image += '⬛';
+            }
+        }
+
+        if (isEmpty){
             break
         }
 
-        const computedStyles = window.getComputedStyle(tile);
-        // Get the background color property from the computed styles
-        const backgroundColor = computedStyles.backgroundColor;
-        if (backgroundColor == 'rgb(83, 141, 78)'){
-            row_image += '🟩';
-        } else if (backgroundColor == 'rgb(181, 159, 59)'){
-            row_image += '🟨';
-        } else {
-            row_image += '⬛';
-        }
+        row_full.push(row_image)
     }
 
-    if (isEmpty){
-        break
+    /* why pop? because for some reason there is an empty element at the end of the list
+    if (row_full[-1]== ''){
+        row_full.pop()
     }
 
-    row_full.push(row_image)
+    */
+
+    console.log(row_full)
+    // this will be the number of attempts taken to complete the wordle. this can be displayed in the end to show the number of attempts taken to complete the wordle.
+    attempts = i;
+
+
+    // for the next part of the code
+    // the code should print each element in the list row_full in each div with their id = row<index>
+    // one more div should show the number of attempts
+    document.getElementById('row1').textContent = row_full[0];
+    document.getElementById('row2').textContent = row_full[1];
+    document.getElementById('row3').textContent = row_full[2];
+    document.getElementById('row4').textContent = row_full[3];
+    document.getElementById('row5').textContent = row_full[4];
+    document.getElementById('row6').textContent = row_full[5];
+    document.getElementById('attempts').textContent = `${attempts}/6`;
 }
-
-/* why pop? because for some reason there is an empty element at the end of the list
-if (row_full[-1]== ''){
-    row_full.pop()
-}
-
-*/
-
-console.log(row_full)
-// this will be the number of attempts taken to complete the wordle. this can be displayed in the end to show the number of attempts taken to complete the wordle.
-attempts = i;
-
-
-// for the next part of the code
-// the code should print each element in the list row_full in each div with their id = row<index>
-// one more div should show the number of attempts
