5e1d0aeed5955c889c49ffff39bc97700b869c1b
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-16 19:14:17 +0530

    testing

--- content.js
+++ content.js
@@ -1,44 +1,42 @@
 console.log('hello');
 
 window.onload = () => {
-
-
     console.log('website loaded');
 
     var check = function(){
         const b2 = document.querySelector("body > div:nth-child(2) > div > div > div > div > div.Welcome-module_buttonContainer__K4GEw > button:nth-child(3)");
         if (b2) {
-            // Click the x button
+            // Click the play button
             b2.click();
             console.log('clicked play');
-            // Perform your action here after the button is clicked
+            // Once the play button is clicked, check for the X button
+            setTimeout(checkXButton, 1000); // Wait for a second before checking for X button
         } else {
-            // If the button is not found, wait and check again
+            // If the play button is not found, wait and check again
             setTimeout(check, 1000); // Check again in a second
         }
     }
-    
-    check();
 
-    var check2 = function(){
+    var checkXButton = function(){
         const xbutton = document.querySelector("#help-dialog > div > div > button");
         if (xbutton) {
-            // Click the x button
+            // Click the X button
             xbutton.click();
-            console.log('Clicked x button');
+            console.log('Clicked X button');
             // Perform your action here after the button is clicked
         } else {
-            // If the button is not found, wait and check again
-            setTimeout(check2, 1000); // Check again in a second
+            // If the X button is not found, wait and check again
+            setTimeout(checkXButton, 1000); // Check again in a second
         }
     }
-    
-    check2();
-    
 
+    // Start checking for the play button
+    check();
+
+    console.log('about to add the button');
+
+    // Create a button dynamically
     const button = document.createElement("button");
-
-    // Set button attributes and styles
     button.textContent = "Generate Photo";
     button.style.color = "white";
     button.style.backgroundColor = "rgb(83, 141, 78)";
@@ -47,12 +45,10 @@ window.onload = () => {
     button.style.border = "none";
     button.style.margin = "8px";
 
-    // Find the appropriate location to insert the button on the webpage and append it
+    // Append the button to the header once the page is loaded
     const heading = document.querySelector("body > div:nth-child(2) > div > div.App-module_gameContainer__K_CBh > header");
     if (heading) {
-    heading.appendChild(button);
+        heading.appendChild(button);
+        console.log('Button added');
     }
-
-    console.log('button added');
-
 }
