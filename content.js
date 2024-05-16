3659a538991dab16e41928c25a1a4b6406524d6a
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-16 19:10:41 +0530

    testing

--- content.js
+++ content.js
@@ -2,16 +2,39 @@ console.log('hello');
 
 window.onload = () => {
 
+
     console.log('website loaded');
-    const b2 = document.querySelector("body > div:nth-child(2) > div > div > div > div > div.Welcome-module_buttonContainer__K4GEw > button:nth-child(3)");
-    b2.click();
 
-    console.log('clicked play');
+    var check = function(){
+        const b2 = document.querySelector("body > div:nth-child(2) > div > div > div > div > div.Welcome-module_buttonContainer__K4GEw > button:nth-child(3)");
+        if (b2) {
+            // Click the x button
+            b2.click();
+            console.log('clicked play');
+            // Perform your action here after the button is clicked
+        } else {
+            // If the button is not found, wait and check again
+            setTimeout(check, 1000); // Check again in a second
+        }
+    }
+    
+    check();
 
-    const xbutton = document.querySelector("#help-dialog > div > div > button");
-    xbutton.click();
-
-    console.log('clicked x');
+    var check2 = function(){
+        const xbutton = document.querySelector("#help-dialog > div > div > button");
+        if (xbutton) {
+            // Click the x button
+            xbutton.click();
+            console.log('Clicked x button');
+            // Perform your action here after the button is clicked
+        } else {
+            // If the button is not found, wait and check again
+            setTimeout(check2, 1000); // Check again in a second
+        }
+    }
+    
+    check2();
+    
 
     const button = document.createElement("button");
 
