1b99c320fb0722e9f298e93450abed07cafa487b
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-05-16 14:13:51 +0530

    testing

--- content.js
+++ content.js
@@ -1,31 +1,36 @@
 console.log('hello');
 
-const b2 = document.querySelector("body > div:nth-child(2) > div > div > div > div > div.Welcome-module_buttonContainer__K4GEw > button:nth-child(3)");
-b2.click();
+window.onload = () => {
 
-console.log('clicked play');
+    console.log('website loaded');
 
-const xbutton = document.querySelector("#help-dialog > div > div > button");
-xbutton.click();
+    const b2 = document.querySelector("body > div:nth-child(2) > div > div > div > div > div.Welcome-module_buttonContainer__K4GEw > button:nth-child(3)");
+    b2.click();
 
-console.log('clicked x');
+    console.log('clicked play');
 
-const button = document.createElement("button");
+    const xbutton = document.querySelector("#help-dialog > div > div > button");
+    xbutton.click();
 
-// Set button attributes and styles
-button.textContent = "Generate Photo";
-button.style.color = "white";
-button.style.backgroundColor = "rgb(83, 141, 78)";
-button.style.padding = "10px";
-button.style.borderRadius = "50px";
-button.style.border = "none";
-button.style.margin = "8px";
+    console.log('clicked x');
 
-// Find the appropriate location to insert the button on the webpage and append it
-const heading = document.querySelector("body > div:nth-child(2) > div > div.App-module_gameContainer__K_CBh > header");
-if (heading) {
-  heading.appendChild(button);
+    const button = document.createElement("button");
+
+    // Set button attributes and styles
+    button.textContent = "Generate Photo";
+    button.style.color = "white";
+    button.style.backgroundColor = "rgb(83, 141, 78)";
+    button.style.padding = "10px";
+    button.style.borderRadius = "50px";
+    button.style.border = "none";
+    button.style.margin = "8px";
+
+    // Find the appropriate location to insert the button on the webpage and append it
+    const heading = document.querySelector("body > div:nth-child(2) > div > div.App-module_gameContainer__K_CBh > header");
+    if (heading) {
+    heading.appendChild(button);
+    }
+
+    console.log('button added');
+
 }
-
-console.log('button added');
-
